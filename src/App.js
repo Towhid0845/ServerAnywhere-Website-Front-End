a48b0631074e3a98f21f46c095cb7f1216dcdc4f
Towhid <towhid0845@gmail.com>
2022-09-08 00:20:03 +0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<div class="header__logo">
+				<a href="/" class="logo" asd="true">
+					<img
+						width="140"
+						height="46"
+						src="/images/logo-dark-theme.svg?569969ccf0a406ad600a650a6f1e8670"
+						alt="logo"
+						loading="lazy"
+					/>
+				</a>
+			</div>
+		</div>
+	);
 }
 
 export default App;
