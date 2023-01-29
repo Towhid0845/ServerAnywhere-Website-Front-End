4c267b9d01c71c81f9c8e61d4ce1a9224af7774f
Towhid <towhid0845@gmail.com>
2023-01-29 16:07:03 +0600

    This is last commit

--- src/components/admin/game/gameserver.js
+++ src/components/admin/game/gameserver.js
@@ -4,10 +4,13 @@ import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import { Button } from "@mui/material";
 import { useState } from "react";
-// import { Link } from "react-router-dom";
-var port = 8010;
-var user = 109;
-var allocation = 1000;
+
+var port = 25529;
+var user = 140;
+var allocation = 0;
+var totalObj = 98;
+var pageNumber = Math.ceil(totalObj / 50);
+// const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export default function Gameserver() {
 	const initialValues = {
@@ -24,33 +27,27 @@ export default function Gameserver() {
 	};
 
 	const token = "ptla_AZJBhkEZtKfVwMEnHW84JmHHZ5oKSplOKO2QhGooxBl";
-	// const encodedToken = Buffer.from(token).toString("base64");
 	let axiosConfig = {
 		headers: {
-			//  "Access-Control-Allow-Origin": "*",
-			// "Access-Control-Allow-Headers": "*",
-			// "Accept": "application/json",
-			// "Content-Type": "application/json",
 			Authorization: "Bearer " + token,
 		},
-		// },
-		// token: {
-		// 	pterodactyl_session:
-		// 		"eyJpdiI6InhIVXp5ZE43WlMxUU1NQ1pyNWRFa1E9PSIsInZhbHVlIjoiQTNpcE9JV3FlcmZ6Ym9vS0dBTmxXMGtST2xyTFJvVEM5NWVWbVFJSnV6S1dwcTVGWHBhZzdjMHpkN0RNdDVkQiIsIm1hYyI6IjAxYTI5NDY1OWMzNDJlZWU2OTc3ZDYxYzIyMzlhZTFiYWY1ZjgwMjAwZjY3MDU4ZDYwMzhjOTRmYjMzNDliN2YifQ%3D%3D",
-		// },
 	};
+
 	const postinfo = (e) => {
 		e.preventDefault();
-		console.log(formValues);
+		// console.log(formValues);
+		// allocation++;
 		port++;
 		user++;
-		allocation++;
+		totalObj++;
 		console.log(port);
 		console.log(user);
-		console.log(allocation);
+		console.log(totalObj);
+		console.log(pageNumber);
+
 		Promise.all([
+			//game server user creation
 			axios.post(
-				//game server user creation
 				"https://test-panel.serveranywhere.net/api/application/users",
 				{
 					username: formValues.username,
@@ -61,9 +58,9 @@ export default function Gameserver() {
 				},
 				axiosConfig
 			),
-			// setTimeout(function() {
+			// game server allocation
+			// setTimeout(() => {
 			axios.post(
-				// game server allocation
 				"https://test-panel.serveranywhere.net/api/application/nodes/2/allocations",
 				{
 					ip: "103.191.240.110",
@@ -71,19 +68,14 @@ export default function Gameserver() {
 				},
 				axiosConfig
 			),
-			// }, 5000),
-			axios.get(
-				// game server allocation
-				"https://test-panel.serveranywhere.net/api/application/nodes/2/allocations",
-				axiosConfig
-			),
-			// .then((res) => {
-			// 	const alloc = res.data.attributes.assigned;
-			// 	console.log(alloc);
-			// }),
-
+			// }, [5000]), // 5 second
+			// getting game server allocation id
+			// axios.get(
+			// 	`https://test-panel.serveranywhere.net/api/application/nodes/2/allocations?page=${pageNumber}`,
+			// 	axiosConfig
+			// ),
+			// send mail to the user
 			// axios.post(
-			// 	// send mail to the user
 			// 	"http://103.191.240.74:81/api/admin/email/send",
 			// 	{
 			// 		to: formValues.email,
@@ -98,80 +90,94 @@ export default function Gameserver() {
 		])
 			.then(
 				axios.spread((res1, res2, res3) => {
-					// axios.spread((res1, res2, res3, res4) => {
 					console.log(res1.data);
 					console.log(res2.data);
 					// console.log(res3.data);
+
+					// const temp = res3.data;
+					// const temp2 = temp.meta.pagination.count - 1;
+					// console.log(temp2);
+					// console.log(temp.data[temp2]);
+					// allocation = temp.data[temp2].attributes.id;
+					// console.log(allocation);
+					// alert("Success......");
 					// console.log(res3.data.items[1]);
 					// const xy = temp.length;
-					// console.log(Object.keys(temp.data));
-					const temp = res3.data;
-					console.log(temp);
 					// const xy = Object.keys(temp.data).length;
-					// console.log(xy);
-					const temp3 = temp.meta.pagination.total - 1;
-					console.log(temp3);
-					console.log(temp.data[temp3]);
+					// console.log(Object.keys(temp.data));
 					// const temp2 = temp.data[temp3].attributes.assigned;
 					// console.log(temp2);
-					// if (temp2 === false) {
-					// 	allocation = temp.data[temp3].attributes.id;
-					// 	console.log(allocation);
-					// 	alert("Success......");
-					// }
-					// console.log(res4.data);
 				})
 			)
 			.catch((error) => {
 				console.log(error);
 			});
-		// port++;
-		// user++;
-		// allocation++;
+		// totalObj++;
 	};
+	const getAllocation = (e) => {
+		e.preventDefault();
+		axios
+			.get(
+				`https://test-panel.serveranywhere.net/api/application/nodes/2/allocations?page=${pageNumber}`,
+				axiosConfig
+			)
+			.then((res3) => {
+				console.log(res3.data);
+				const temp = res3.data;
+				const temp2 = temp.meta.pagination.count - 1;
+				console.log(temp2);
+				console.log(temp.data[temp2]);
+				allocation = temp.data[temp2].attributes.id;
+				console.log(allocation);
+			});
+	};
+
 	const serverinfo = (e) => {
 		e.preventDefault();
-		setTimeout(function() {
-			//your code to be executed after 1 second
-			axios
-				.post(
-					// server creattion
-					"https://test-panel.serveranywhere.net/api/application/servers",
-					{
-						name: "Building35",
-						user: user,
-						egg: 1,
-						docker_image: "ghcr.io/pterodactyl/yolks:java_17",
-						startup: "java -Xms128M -XX:MaxRAMPercentage=95.0 -jar",
-						environment: {
-							BUNGEE_VERSION: "latest",
-							SERVER_JARFILE: "bungeecord.jar",
-						},
-						limits: {
-							memory: 128,
-							swap: 0,
-							disk: 512,
-							io: 500,
-							cpu: 0,
-						},
-						feature_limits: {
-							databases: 5,
-							backups: 1,
-						},
-						// allocation: allocation,
-						allocation: {
-							default: allocation,
-						},
+		// setTimeout(function() {
+		// server creattion
+		axios
+			.post(
+				"https://test-panel.serveranywhere.net/api/application/servers",
+				{
+					name: "Building35",
+					user: user,
+					egg: 1,
+					docker_image: "ghcr.io/pterodactyl/yolks:java_17",
+					startup: "java -Xms128M -XX:MaxRAMPercentage=95.0 -jar",
+					environment: {
+						BUNGEE_VERSION: "latest",
+						SERVER_JARFILE: "bungeecord.jar",
 					},
-					axiosConfig
-				)
-				.then((res) => {
+					limits: {
+						memory: 128,
+						swap: 0,
+						disk: 512,
+						io: 500,
+						cpu: 0,
+					},
+					feature_limits: {
+						databases: 5,
+						backups: 1,
+					},
+					// allocation: allocation,
+					allocation: {
+						default: allocation,
+					},
+				},
+				axiosConfig
+			)
+			.then((res) => {
+				setTimeout(() => {
 					console.log(res);
-				})
-				.catch((error) => {
-					console.log(error);
-				});
-		}, 5000);
+				}, 5000);
+
+				window.location.reload(true);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+		// }, 5000);
 	};
 	return (
 		<div>
@@ -299,16 +305,10 @@ export default function Gameserver() {
 										name="password"
 									/>
 								</InputGroup>
-								{/* <p className="text-danger">{formErrors.password}</p> */}
+
 								<br />
 
-								{/* <p className="text-danger">{formErrors.password_confirm}</p> */}
-								{/* <br /> */}
-								{/* <div className="text-denger error">{isError}</div> */}
-
 								<div className="mx-auto text-center">
-									{/* {error && <div className={styles.error_msg}>{error}</div>} */}
-									{/* {msg && <div className={styles.success_msg}>{msg}</div>} */}
 									<Button
 										style={{ backgroundColor: "#f74545" }}
 										className="button-34 ps-5 pe-5 pt-2 pb-2"
@@ -316,6 +316,16 @@ export default function Gameserver() {
 										type="submit"
 									>
 										Sign Up
+									</Button>
+									<br />
+									<br />
+									<Button
+										style={{ backgroundColor: "#f74545" }}
+										className="button-34 ps-5 pe-5 pt-2 pb-2"
+										onClick={getAllocation}
+										type="submit"
+									>
+										Get Allocation
 									</Button>
 									<br />
 									<br />
